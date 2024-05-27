36de4e44f111f288afcd569b98163db59965bafa
seregachempion22 <48333844+segerachempion22@users.noreply.github.com>
2024-05-28 02:41:59 +0300

    Update temp.js

--- temp.js
+++ temp.js
@@ -1,9 +1,1 @@
-const dateString = "Fri Nov 17 2023 14:59:38 GMT+0300 (Москва, стандартное время)";
 
-// Создание объекта Date из строки даты
-const date = new Date(dateString);
-
-// Получение Unix Timestamp
-const timestamp = date.getTime();
-
-console.log(timestamp);
